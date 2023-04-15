6430f7dda9e4659649581fb8e6aa78090fd5c068
LONKASHIVA <pardhushiva99@gmail.com>
2023-04-15 16:50:56 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./Nav";
+import Main2 from "./Main2";
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <Main2 />
+    </>
+  );
+}
---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <>
+      <nav>
+        <img src="src/images/react-logo.png" alt="react" />
+        <h3 className="nav-logo">ReactFacts</h3>
+        <h4 className="nav-title">React Course - project 1</h4>
+      </nav>
+    </>
+  );
+}
